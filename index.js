a6d18d224b080d47cb6d0162996f93de20e32163
天然呆幻月 <64715639+Tira-tw@users.noreply.github.com>
2022-07-16 12:54:04 +0000

    a

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 /**
  * Copyright 2021 yan-930521  All Rights Reserved.
  */
-
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -27,7 +26,7 @@ client.on('ready', async () => {
     console.log("尚未建立資料庫\n請在shell中輸入 node build.js 初始化資料庫");
     return;
   }
-  console.log('=====tobanlist=====');
+  console.log('=====停權列表=====');
   for (let banner in tobanlist) {
     console.log(tobanlist[banner].user_id);
   }
@@ -37,46 +36,80 @@ client.on('ready', async () => {
     status: "online"
   }).then(()=>{
     client.user.setActivity(`${config.prefix}${config.commands["HELP"].name} | 
-    ${client.guilds.cache.size} servers`,{
-      type: "PLAYING"
-    });
-  })
+    ${client.guilds.cache.size} 個伺服器`,{
+        type: "PLAYING"
+      });
+    setInterval(()=>{
+      client.user.setActivity(`${config.prefix}${config.commands["HELP"].name} | 
+    ${client.guilds.cache.size} 個伺服器`,{
+        type: "PLAYING"
+      });
+    },12000000);
+  });
 });
 
 client.on("message", async msg => {
   if(!msg.content.startsWith(config.prefix)) return;
   let cmd = msg.content.split(" ")[0].replace(config.prefix, "");
   if (cmd == config.commands['BAN'].name && config.owner_id.includes(msg.author.id)) {
-    if (!msg.member.hasPermission("BAN_MEMBERS")) return msg.channel.send('系統錯誤 請檢查您的權限')
-    if (!msg.guild.me.hasPermission("BAN_MEMBERS")) return msg.channel.send('系統錯誤 請檢查在下的權限')
+    if (!msg.member.hasPermission("BAN_MEMBERS")) return msg.channel.send('系統錯誤 請檢查您的權限');
+    if (!msg.guild.me.hasPermission("BAN_MEMBERS")) return msg.channel.send('系統錯誤 請檢查在下的權限');
 
-    var user_id = msg.content.replace(config.commands['BAN'].name, "");
+    var user_id = msg.content.split(" ")[1].replace(config.commands['BAN'].name, "").replace(/ /g, "").replace(/@/g, "").replace(/</g, "").replace(/!/g, "").replace(/>/g, "");
+    if(user_id.includes("&")) {
+      msg.channel.send('系統錯誤 請輸入正確的用戶');
+      return;
+    }
+    console.log(user_id);
     if(!user_id && msg.mentions.user.first()) {
       user_id = msg.mentions.user.first().id;
-    } else {
+    }
+    if(!user_id && !msg.mentions.user.first()) {
+      msg.channel.send('系統錯誤 請輸入正確的用戶');
+      return;
+    } 
+    let user = await client.users.fetch(user_id);
+    console.log(user);
+    if(!user) {
+      globalSave(user_id);
+      msg.channel.send('系統錯誤 查無使用者 已經該用戶 ID'+user_id+"列入tobanlist");
+      return;
+    }
+    globalBan(client, user, msg.channel, msg.content.replace(msg.content.split(" ")[0], "").replace(msg.content.split(" ")[1], ""));
+    msg.react('✅');
+  }
+  if (cmd == config.commands['BANLIST'].name) {
+    if (!msg.member.hasPermission("BAN_MEMBERS")) return msg.channel.send('系統錯誤 請檢查您的權限');
+    if (!msg.guild.me.hasPermission("BAN_MEMBERS")) return msg.channel.send('系統錯誤 請檢查在下的權限');
+    BanALL(msg);
+    msg.react('✅');
+  }
+
+  if (cmd == config.commands['UNBAN'].name  && config.owner_id.includes(msg.author.id)) {
+    if (!msg.member.hasPermission("UNBAN_MEMBERS")) return msg.channel.send('系統錯誤 請檢查您的權限');
+    if (!msg.guild.me.hasPermission("UNBAN_MEMBERS")) return msg.channel.send('系統錯誤 請檢查在下的權限');
+    var user_id = msg.content.split(" ")[1].replace(config.commands['BAN'].name, "").replace(/ /g, "").replace(/@/g, "").replace(/</g, "").replace(/!/g, "").replace(/>/g, "");
+    if(user_id.includes("&")) {
+      msg.channel.send('系統錯誤 請輸入正確的用戶');
+      return;
+    }
+    if(!user_id && msg.mentions.user.first()) {
+      user_id = msg.mentions.user.first().id;
+    }
+    if(!user_id && !msg.mentions.user.first()) {
       msg.channel.send('系統錯誤 請輸入正確的用戶');
       return;
     }
     let user = await client.users.fetch(user_id);
-    console.log(user);
-    if(!user) {
-      msg.channel.send('系統錯誤 請輸入正確的用戶');
-      return;
-    }
-    globalBan(client, user, msg.channel);
+    UNBanALL(msg, user.id);
     msg.react('✅');
   }
-  if (cmd == config.commands['BANLIST'].name) {
-    if (!msg.member.hasPermission("BAN_MEMBERS")) return msg.channel.send('系統錯誤 請檢查您的權限')
-    if (!msg.guild.me.hasPermission("BAN_MEMBERS")) return msg.channel.send('系統錯誤 請檢查在下的權限')
-    BanALL(msg);
-    msg.react('✅');
-  }
+
   if (cmd == config.commands['SUPPORT'].name) {
     let embed = new Discord.MessageEmbed()
       .setColor(config.embed.color)
       .setTitle('SUPPORT')
-      .setDescription(config.commands['SUPPORT'].description);
+      .setDescription("**[支援區](https://discord.gg/D9hWKmest8)**");
     msg.channel.send(embed);
     msg.react('✅');
   }
@@ -85,7 +118,7 @@ client.on("message", async msg => {
     let embed = new Discord.MessageEmbed()
       .setColor(config.embed.color)
       .setTitle('💻Dev')
-      .setDescription("**黑月Kalo#7107 | 櫻2#0915**");
+      .setDescription("**๛M͜͡r幻月シ#1853 | 櫻2#0915**");
     msg.channel.send(embed);
     msg.react('✅');
   }
@@ -94,7 +127,7 @@ client.on("message", async msg => {
     let embed = new Discord.MessageEmbed()
       .setColor(config.embed.color)
       .setTitle('INVITE')
-      .setDescription("**[LINK](https://discord.com/api/oauth2/authorize?client_id=919442864071643216&permissions=134&scope=bot)**");
+      .setDescription("**[邀請連結](https://discord.com/api/oauth2/authorize?client_id=919442864071643216&permissions=1512097302774&scope=bot)**");
     msg.channel.send(embed);
     msg.react('✅');
   }
@@ -102,7 +135,7 @@ client.on("message", async msg => {
   if (cmd == config.commands['HELP'].name) {
     let embed = new Discord.MessageEmbed()
       .setColor(config.embed.color)
-      .setTitle('Commands list');
+      .setTitle('功能列表');
     for(let cm in config['commands']) {
       embed.addField(
         cm,
@@ -119,39 +152,79 @@ client.on("guildMemberAdd",async m =>{
     if(tobanlist[banner].user_id != m.user.id) continue;
     let user = await m.guild.members.fetch(tobanlist[banner].user_id);
     if (user) {
-      user.ban().catch(err => console.log(err));
+      user.ban({"reason":"你已經列入停權名單 , 只要在有<@919442864071643216>的伺服器裡 , 你將會被停權!\n若有任何意見 , 請到以下群組詢問!\nhttps://discord.gg/D9hWKmest8"}).catch(err => console.log(err));
     }
   }
 });
-async function globalBan(client, u, channel) {
+async function globalSave(id) {
+  let tobanlist = await db.get("bans");
+  tobanlist.push(new Banner(id));
+  db.set("bans", tobanlist);
+  console.log("tobanlist 新增：" + id)
+}
+async function globalBan(client, u, channel, reason) {
   let error = false;
+  let str = "";
   let tobanlist = await db.get("bans");
   tobanlist.push(new Banner(u.id));
   db.set("bans", tobanlist);
   console.log("tobanlist 新增：" + u.id)
   let guilds = [];
-  client.guilds.cache.find(g => guilds.push(g));
-  /// console.log(guilds.length)
+  client.guilds.cache.map(g => guilds.push(g));
   for (let guild in guilds) {
-    console.log(guilds[guild].name)
-    let user = await guilds[guild].members.fetch(u.id);
+    console.log(guilds[guild].name);
+    let user = guilds[guild].members.cache.get(u.id)
     if (user) {
-      await user.ban().catch(err => {
+      user.send(reason);
+      let a = await user.ban({"reason":reason}).catch(err => {
         console.log(err);
-        error = true;
       });
+      if(a) {
+        str += `ban ID ${user.id} from ${guilds[guild].name}\n`;
+      }
     }
   }
-  console.log(error);
+  if(str != "") {
+    channel.send(str);
+  }
 }
-async function BanALL(msg) {
+async function UNBanALL(msg, user_id) {
   let tobanlist = await db.get("bans");
-  // console.log(msg.guild.name);
+  let newlist = [];
+  for(let i in tobanlist) {
+    if(tobanlist[i].user_id == user_id) {
+      continue
+    }
+    newlist.push(tobanlist[i]);
+  }
+  db.set("bans", newlist);
+  client.guilds.cache.map(async g =>{
+    let user = g.members.cache.get(user_id);
+    if(user) {
+      console.log(user)
+      await user.unban();
+    }
+  });
+  msg.channel.send(`正在移除 ${user_id} 用戶ID....`);
+
+}
+
+async function BanALL (msg) {
+  let str = "";
+  let tobanlist = await db.get("bans");
   for (let banner in tobanlist) {
-    let user = await msg.guild.members.fetch(tobanlist[banner].user_id);
+    let user = msg.guild.cache.get(tobanlist[banner].user_id)
     if (user) {
-      user.ban().catch(err => console.log(err));
+      user.send("你已經列入停權名單 , 只要在有<@919442864071643216>的伺服器裡 , 你將會被停權!\n若有任何意見 , 請到以下群組詢問!\nhttps://discord.gg/D9hWKmest8");
+      let a = await user.ban({"reason":"你已經列入停權名單 , 只要在有<@919442864071643216>的伺服器裡 , 你將會被停權!\n若有任何意見 , 請到以下群組詢問!\nhttps://discord.gg/D9hWKmest8"}).catch(err => {
+        console.log(err);
+      });
+      if(a) {
+        str += `ban ID ${user.id}\n`;
+      }
     }
   }
-
+  if(str != "") {
+    msg.channel.send(str);
+  }
 }
